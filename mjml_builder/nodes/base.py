d9e718a1cebbb9f0b3a46041b90ce61f500f6248
Ankur Paudel <paudelankur+github@gmail.com>
2025-12-31 22:22:13 +0000

    feat: implement html generation for nodes

--- mjml_builder/nodes/base.py
+++ mjml_builder/nodes/base.py
@@ -1,6 +1,8 @@
 import importlib
 import importlib.util
+import io
 import shutil
+import subprocess
 
 
 class BaseNode:
@@ -43,6 +45,53 @@ class BaseNode:
 
         markup = f"{tag_start}{content}{tag_end}"
         return markup
+
+    @property
+    def html(self, force_external=False):
+        from .containers import Mjml, Body
+
+        # wrap node with mjml and mj-body tags
+        node = self
+        if not isinstance(node, Mjml) and not isinstance(node, Body):
+            node = Body(node)
+        if not isinstance(node, Mjml):
+            node = Mjml(node)
+
+        # python implementation
+        has_mrml_python = importlib.util.find_spec("mrml") is not None
+        has_mjml_python = importlib.util.find_spec("mjml") is not None
+        # external implementation
+        has_mjml = shutil.which("mjml") is not None
+        has_bun = shutil.which("bun") is not None
+        has_pnpx = shutil.which("pnpx") is not None
+        has_npx = shutil.which("npx") is not None
+
+        if has_mrml_python and not force_external:
+            import mrml  # type: ignore
+
+            return mrml.to_html(node.mjml).content
+        if has_mjml_python and not force_external:
+            import mjml  # type: ignore
+
+            return mjml.mjml_to_html(io.StringIO(node.mjml)).html
+
+        if has_mjml:
+            mjml_command = []
+        elif has_bun:
+            mjml_command = ["bun", "x"]
+        elif has_pnpx:
+            mjml_command = ["pnpx"]
+        elif has_npx:
+            mjml_command = ["npx"]
+        else:
+            raise OSError("mjml command not found")
+
+        mjml_command += ["mjml", "--stdin", "--stdout"]
+        output = subprocess.check_output(
+            mjml_command,
+            input=node.mjml.encode(),
+        ).decode()
+        return output
 
 
 class StemNode(BaseNode):
