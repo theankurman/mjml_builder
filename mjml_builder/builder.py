bf35516e87b1366589d35fca385d0c3138c1a37c
Ankur Paudel <paudelankur+github@gmail.com>
2026-01-02 10:21:27 +0000

    docs: add docstrings to Builder class and methods

--- mjml_builder/builder.py
+++ mjml_builder/builder.py
@@ -4,12 +4,28 @@ from .components.elements import Text
 
 
 class Builder:
+    """Used to build an email.
+
+    Attributes:
+        sections (list[Section]): A list of ``mj-section`` tags that this builder contains.
+    """
+
     sections: list[Section]
 
     def __init__(self) -> None:
         self.sections = []
 
     def add_section(self, *sections: Section, **attributes: str):
+        """Add the given sections to the builder.
+
+        If no sections are provided, add an empty section.
+
+        Args:
+            *sections: list of :class:`Section` s to add as positional arguments.
+
+                Can be empty to automatically add an empty section.
+            **attributes: attributes to add to the generated empty section if no sections have been provided.
+        """
         new_sections = list(sections)
         if not new_sections:
             new_sections.append(Section(**attributes))
@@ -18,6 +34,15 @@ class Builder:
         return self
 
     def add_column(self, *columns: Column, **attributes: str):
+        """Add a column to the last section in the builder.
+
+        Args:
+            *columns: list of :class:`Column` s to add, provided as positional arguments.
+
+                Can be left empty to automatically add an empty column
+            **attributes: attributes to add to the automatically generated
+                empty column if no columns have been provided.
+        """
         # create section if it doesnt exist
         if not self.sections:
             self.add_section()
@@ -31,6 +56,17 @@ class Builder:
         return self
 
     def add_content(self, *contents: BaseComponent | str, **attributes):
+        """Add content to the last column in the last section in the builder.
+
+        Args:
+            *contents: A list of contents to add to the column, provided as positional args.
+
+                - If an instance of :class:`BaseComponent`, it is added as is.
+                - If an instance of :class:`str`, it is wrapped with a :class:`Text`
+                    and provided attributes are applied to the :class:`Text` tag.
+
+            **attributes: attributes to apply to the :class:`Text` tag when a string is passed in as content.
+        """
         # if content is str, change to text component
         new_contents = []
         for content in contents:
@@ -51,5 +87,6 @@ class Builder:
         return self
 
     def build(self):
+        """Build the email component."""
         # wrap sections in mjml and body and return component
         return Mjml(Body(*self.sections))
